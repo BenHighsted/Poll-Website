0bc028c7383846e3e31a832f545afbf467d620b7
BenHighsted <benhigh18@gmail.com>
2020-09-28 15:56:52 +1300

    First commit, basic files setup, .gitignore

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * 
+ * Thinking creating a Poll website could be a good 'first project back'.
+ * Would be a good one to use version control with, and make it well documented.
+ * 
+ * As I have been working with similar technologies at my internship, should be pretty
+ * easy to just apply what I have been doing to a different project.
+ * 
+ * Could attempt to make it similar to stockpoll (or whatever its called)
+ * but have it so it can display the data in all different formats (like pi charts and such)
+ * 
+ * Will need to make sure I .gitignore the connect.php file once I have created it.
+ * 
+ */
+
+?>
+
+<!DOCTYPE html>
+
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style/style.css">
+        <script type="text/javascript" src="js/index.js"></script>
+        <title>Poll Website</title>
+    </head>
+
+    <body>
+        <h1> Welcome to 'Poll Website'! </h1>
+    </body>
+
+</html>
