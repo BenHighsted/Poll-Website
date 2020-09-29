a364c505aeaa877fab73e7fc0e9e911566b46491
BenHighsted <benhigh18@gmail.com>
2020-09-29 18:10:43 +1300

    Changing method from php to javascript

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// A function that creates a new option with a correlating number
+
+function new_option(){
+  var btn = document.createElement("OPTION");
+  btn.innerHTML = "CLICK ME";
+  document.body.appendChild(btn);
+}
